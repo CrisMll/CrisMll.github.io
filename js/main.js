71717afb1de1fc81763769102216b2545d3513fc
CrisMLL <crismll.dev@gmail.com>
2024-03-06 15:18:37 +0100

    estilos para nav-bar y responsive

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//controles javascript para estilar el menú de navegación responsive
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
