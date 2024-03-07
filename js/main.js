688f7322fed61984ff4424b4f4e73501beede0ab
CrisMLL <crismll.dev@gmail.com>
2024-03-07 14:37:09 +0100

    adding buttons and js functions in about me section

--- js/main.js
+++ js/main.js
@@ -20,3 +20,38 @@ function showSection() {
         document.getElementById('hero').classList.add('appear');
     }, 500);
 }
+
+//agregando funciones js para botones de redes
+const githubButton = document.querySelector("#github");
+githubButton.addEventListener("click", () => {
+    const githubUrl = "https://github.com/CrisMll";
+    window.open(githubUrl, "_blank");
+})
+
+const linkedinButton = document.querySelector("#linkedin");
+linkedinButton.addEventListener("click", () => {
+    const linkedinUrl = "https://www.linkedin.com/in/crismll/";
+    window.open(linkedinUrl, "_blank");
+})
+
+const cvButton = document.querySelector("#cv");
+cvButton.addEventListener("click", () => {
+    const cvUrl = "";
+    window.open(cvUrl, "_blank");
+})
+
+const emailLink = document.getElementById('emailLink');
+const clipboardIcon = document.getElementById('email-clipboard');
+
+clipboardIcon.addEventListener('click', () => {
+    const tempInput = document.createElement('input');
+    tempInput.value = emailLink.innerText;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Correo electrónico copiado: ' + emailLink.innerText);
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
