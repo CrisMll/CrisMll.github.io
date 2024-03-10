bfbcc14c293c12e92625bccd6f69354b0e2ff7f8
CrisMLL <crismll.dev@gmail.com>
2024-03-10 20:54:50 +0100

    fixing js copy-email function

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ const close = document.querySelector("#close");
 
 open.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+});
 
 close.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+});
 
 
 //funcion para la animación del hero
@@ -19,42 +19,40 @@ function showSection() {
     setTimeout(function() {
         document.getElementById('hero').classList.add('appear');
     }, 500);
-}
+};
 
 //agregando funciones js para botones de redes
 const githubButton = document.querySelector("#github");
 githubButton.addEventListener("click", () => {
     const githubUrl = "https://github.com/CrisMll";
     window.open(githubUrl, "_blank");
-})
+});
 
 const linkedinButton = document.querySelector("#linkedin");
 linkedinButton.addEventListener("click", () => {
     const linkedinUrl = "https://www.linkedin.com/in/crismll/";
     window.open(linkedinUrl, "_blank");
-})
+});
 
 const cvButton = document.querySelector("#cv");
 cvButton.addEventListener("click", () => {
     const cvUrl = "";
     window.open(cvUrl, "_blank");
-})
+});
 
 
 //COPIAR EMAIL
 
-const emailLink = document.getElementById('emailLink');
-const clipboardIcon = document.getElementById('email-clipboard');
-
-clipboardIcon.addEventListener('click', () => {
-    navigator.clipboard.writeText(emailLink.innerText)
-    .then(() => {
-        alert('Correo electrónico copiado: ' + emailLink.innerText);
-    })
-    .catch(err => {
-        console.error('Error al copiar el correo electrónico: ', err);
-    });
-});
-
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function copyEmail(){
+    let url = document.getElementById('email-link');
+    let button = document.getElementById('copyEmail-button'); 
+
+    navigator.clipboard.writeText(url.textContent);
+    alert ('Email copiado');
+}
+
+const tootltipEx = document.getElementById('copyEmail-button');
+const tooltip = new bootstrap.Tooltip(tootltipEx, options);
